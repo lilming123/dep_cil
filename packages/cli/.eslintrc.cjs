1ba43286bdbce785effedbc2150df7f57199cd01
Plumbiu <3434909403@qq.com>
2023-07-27 19:34:12 +0800

    feat: 🚀 完善 cli

---
+++ packages/cli/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: '@antfu',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'vue'],
+  rules: {},
+}
